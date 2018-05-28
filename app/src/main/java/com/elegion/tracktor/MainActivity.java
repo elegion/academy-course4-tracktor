7f926a8a9f6ca4068f596a0ae7c98904a5088be5
tanchuev <tanchuev@gmail.com>
2018-05-28 16:36:59 +0500

    added permissions

--- app/src/main/java/com/elegion/tracktor/MainActivity.java
+++ app/src/main/java/com/elegion/tracktor/MainActivity.java
@@ -1,15 +1,24 @@
 package com.elegion.tracktor;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LOCATION_REQUEST_CODE = 99;
 
     @BindView(R.id.mapContainer) FrameLayout mapContainer;
     @BindView(R.id.counterContainer) FrameLayout counterContainer;
@@ -42,5 +51,48 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //mMap.setMyLocationEnabled(true);
+            Toast.makeText(this, "Разрешения уже получены!", Toast.LENGTH_SHORT).show();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Запрос разрешений на получение местоположения")
+                    .setMessage("Нам необходимо знать Ваше местоположение, чтобы приложение работало.")
+                    .setPositiveButton("ОК", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //Prompt the user once explanation has been shown
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    LOCATION_REQUEST_CODE);
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0].equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //mMap.setMyLocationEnabled(true);
+                Toast.makeText(this, "Вы дали разрешения!", Toast.LENGTH_SHORT).show();
+            } else {
+                // Permission was denied. Display an error message.
+                Toast.makeText(this, "Вы не дали разрешения!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
